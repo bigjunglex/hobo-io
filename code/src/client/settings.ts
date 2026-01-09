70762b85fcd14454566bf9b5bc34ca8e259ac1bf
Jungle Smashfist <gearl00taz@gmail.com>
2026-01-09 23:51:11 +0300

    hide chat option

--- code/src/client/settings.ts
+++ code/src/client/settings.ts
@@ -1,5 +1,10 @@
+import { setChatHidden } from "./chat";
+
 const soundBtn = document.getElementById('sound-btn')!;
 const soundStatus = document.getElementById('sound-status')!;
+
+const chatBtn = document.getElementById('chat-btn')!;
+const chatStatus = document.getElementById('chat-status')!;
 
 soundBtn.addEventListener('click', () => {
     const isOn = soundStatus.textContent === 'ON';
@@ -9,3 +14,10 @@ soundBtn.addEventListener('click', () => {
 export function getSoundState(): boolean {
     return soundStatus.textContent === 'ON';
 }
+
+chatBtn.addEventListener('click', () => {
+    const isOn = chatStatus.textContent === 'ON';
+    chatStatus.textContent = isOn ? 'OFF' : 'ON';
+
+    setChatHidden(!isOn)
+})
